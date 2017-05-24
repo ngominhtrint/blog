6987cf4a64d45ff7c263418c90f2945dc9f5f215
Tri Ngo <ngominhtrint@gmail.com>
2017-05-24 17:15:23 +0700

    Initial project. First commit

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+
+  def self.search(search)
+    where("title || body ILIKE ?", "%#{search}%")
+  end
+end

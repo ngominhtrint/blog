4e063f76e39ea5bd6a746e6a6205542707e16b2d
Tri Ngo <ngominhtrint@gmail.com>
2017-06-01 14:47:34 +0700

    fix signout link

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ devise_for :users
     resources :comments
     resources :tags
   end
+  match 'users/sign_out' => "devise/sessions#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 

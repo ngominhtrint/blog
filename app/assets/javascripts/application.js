5eb0c55c73ce46a714310ff3c7470f8305ffc11f
Tri Ngo <ngominhtrint@gmail.com>
2017-06-06 17:27:43 +0700

    Update order of js file in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require jquery
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require rails_emoji_picker
-//= require jquery_ujs
